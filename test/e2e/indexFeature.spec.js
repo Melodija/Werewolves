f5cfe76f6f42d8b0f4df171ebc618a38161173d4
Ruta <ruta@baltiejus.com>
2016-06-07 13:48:38 +0100

    tests created

---
+++ test/e2e/indexFeature.spec.js
@@ -0,0 +1,20 @@
+describe("werewolvesApp", function () {
+  it("should get home page title", function() {
+    browser.get('/');
+    expect(browser.getTitle()).toEqual("Werewolves");
+  });
+
+  it('should have a title', function () {
+    browser.get('/');
+    expect($('#header').getText()).toMatch('Werewolves');
+  });
+
+  it('should have a Rules button', function () {
+    browser.get('/');
+    expect($('#rules-btn').getText()).toMatch('Rules');
+
+    $('#rules-btn').click();
+    expect(browser.currentUrl()).toEqual('http://localhost:8080/rules');
+  });
+
+});
---
+++ test/e2e/rulesFeature.spec.js
@@ -0,0 +1,23 @@
+describe("rules", function () {
+
+  it('should have the basic instructions written', function () {
+    browser.get('/rules');
+    expect($('#instructions').getText()).toMatch('test');
+  });
+
+  it('should have the images of the cards available', function () {
+    browser.get('/rules');
+    villager = element(by.css("img[src*='villager.jpg']"));
+    expect((vilager).isPresent()).toBe(true);
+  });
+
+  it('should be able to click on the cards', function () {
+    browser.get('/rules');
+    villager = element(by.css("img[src*='villager.jpg']"));
+    villager.click();
+    expect(browser.currentUrl()).toEqual('http://localhost:8080/rules/villager');
+  });
+
+
+
+});
