2633a83b9b0916f148036de87344f185f69e90c3
Ruta <ruta@baltiejus.com>
2016-06-09 15:02:55 +0100

    attempting to create a service

--- app/js/controllers/setupController.js
+++ app/js/controllers/setupController.js
@@ -10,4 +10,14 @@ wwApp.controller('SetupController', function() {
     console.log(this.playerNumber);
     return Math.floor(numberofPlayers / 4);
   };
+
+  // self.specialCharacterPicker = function(specials) {
+  //   // specials is an array
+  //   // wolves are the num_of_wolves
+  //   // players are the total number of players in the Game
+  //   // totalCharacters = specials;
+  //   // wolves.times
+  //   // totalCharacters.push("wolf" * wolves)
+  //
+  // };
 });
