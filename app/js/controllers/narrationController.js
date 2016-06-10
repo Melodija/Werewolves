561b7ccddd08670dede64b6a9b2335381e7a96bf
Ruta <ruta@baltiejus.com>
2016-06-10 12:20:21 +0100

    everything is written up: rules, narration, cards

--- app/js/controllers/narrationController.js
+++ app/js/controllers/narrationController.js
@@ -31,9 +31,28 @@ wwApp.controller('narrationController', function($scope, deckService) {
   };
 
   self.narrativeStore = {
-    werewolves: 'Please all agree on a person you would like to devour tonight.',
-    villager: 'Discuss who died. Then please nominate who you would like to lynch today. The person with the most votes shall day today.',
-    seer: 'Please select someone whose card you would like to see.'
+    Thief: "*Show the two faced down cards in the middle* Would you like to exchange your persona for one of these cards? You will take on this character for the rest of the game.",
+    Actor: "Would you like to become one of the personas shown in the middle? *If they pick a card, remove that card from the middle*",
+    Cupid: "Please point to two people whom you would like to become lovers.",
+    Lovers: "Lovers, please acknowledge each other. You are now bound by your love and must protect each other until the end of time. Reminder: if one of you die, the other dies from heartbreak.",
+    Seer: "Please select someone whose card you would like to see. *Show them the card*",
+    Fox: "Would you like to choose a person to find out whether or not them, or the people sitting next to each other, are a werewolf? Reminder: if none of the 3 people are a werewolf, you lose your power for the rest of the game.",
+    StutteringJudge: "Please indicate to me a sign that you will show me during the vote if you would like a second vote to commence in the day.",
+    TwoSisters: "Please awake and acknowledge your sibling. You must protect each other.",
+    ThreeBrothers: "Please awake and acknowledge your siblings. You must protect each other.",
+    WildChild: "Please select someone to idolise. You will continue to idolise them for the rest of the game. Reminder: if they die, you will turn into a werewolf.",
+    WolfHound: "Please indicate with a smile to me whether you would like to be a villager or an angry face if you would prefer to be a werewolf for the game. ",
+    BearTamer: "Please awake so I can acknowledge you. Reminder: I will do (a sign / noise) in the day if one of the people sitting next to you is a Werewolf.",
+    Elder: "Please awake so I may acknowledge who you are. Reminder: If a wolf attempts to devour you, you will not die the first time. If you die, you can assume it has happened twice or it wasn't a wolf who killed you.",
+    Defender: "Please select someone you would like to defend from the werewolves tonight. Reminder: You cannot defend the same person two nights in a row.",
+    Werewolves: "Please all agree on a person you would like to devour tonight.",
+    WhiteWolf: "Please indicate who else you would like to kill. Reminder: You may kill a fellow Werewolf if you wish.",
+    DaddyWolf: "*Check if they raise their hand after the wolves have gone to sleep.*",
+    BigBadWolf: "*If no Werewolves / Wild Child / Wolf Hound have been eliminated:* Please point to another person you would like to devour tonight. *If one of those characters has died:* You can no longer use your power because a (x) has died.",
+    Witch: "Would you like to kill anyone? Would you like to save the damned? *Point to the dead*",
+    Piper: "Please pick two people you would like to charm tonight. Reminder: You cannot charm yourself or already charmed people.",
+    Charmed: "Tap the people who have been charmed to let them know about this. They don't acknowledge each other.",
+    Villagers: 'Everyone awakes apart from (insert person name here). Discuss who died. Then please nominate who you would like to lynch today. The person with the most votes shall die today. *Remember to grunt if the Bear Tamer is in play.* *Remember to keep an eye on Stuttering Judge and Devoted Servant if they are in play during the vote*',
   };
 
   self.getNarrative = function() {
--- app/js/controllers/setupController.js
+++ app/js/controllers/setupController.js
@@ -51,4 +51,95 @@ self.objToArray = function (object) {
   //   self.confirmedRoles = selectedRoles;
   //   console.log(self.confirmedRoles);
   // };
+
+  self.roles = [
+              {title: 'Thief',
+              orderNumber: 1,
+              dayOne: true},
+
+              {title: 'Actor',
+              orderNumber: 2,
+              dayOne: false},
+
+              {title: 'Cupid',
+              orderNumber: 3,
+              dayOne: true},
+
+              {title: 'Lovers',
+              orderNumber: 4,
+              dayOne: true},
+
+              {title: 'Seer',
+              orderNumber: 5,
+              dayOne: false},
+
+              {title: 'Fox',
+              orderNumber: 6,
+              dayOne: false},
+
+              {title: 'StutteringJudge',
+              orderNumber: 7,
+              dayOne: true},
+
+              {title: 'TwoSisters',
+              orderNumber: 8,
+              dayOne: true},
+
+              {title: 'ThreeBrothers',
+              orderNumber: 9,
+              dayOne: true},
+
+              {title: 'WildChild',
+              orderNumber: 10,
+              dayOne: true},
+
+              {title: 'WolfHound',
+              orderNumber: 11,
+              dayOne: true},
+
+              {title: 'BearTamer',
+              orderNumber: 12,
+              dayOne: true},
+
+              {title: 'Elder',
+              orderNumber: 13,
+              dayOne: true},
+
+              {title: 'Defender',
+              orderNumber: 14,
+              dayOne: false},
+
+              {title: 'Werewolves',
+              orderNumber: 15,
+              dayOne: false},
+
+              {title: 'WhiteWolf',
+              orderNumber: 16,
+              dayOne: false},
+
+              {title: 'DaddyWolf',
+              orderNumber: 17,
+              dayOne: false},
+
+              {title: 'BigBadWolf',
+              orderNumber: 18,
+              dayOne: false},
+
+              {title: 'Witch',
+              orderNumber: 19,
+              dayOne: false},
+
+              {title: 'Piper',
+              orderNumber: 20,
+              dayOne: false},
+
+              {title: 'Charmed',
+              orderNumber: 21,
+              dayOne: false},
+
+              {title: 'Villagers',
+              orderNumber: 22,
+              dayOne: false},
+
+            ];
 });
